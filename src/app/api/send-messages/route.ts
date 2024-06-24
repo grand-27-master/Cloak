8c6f5d826672e127afe0cfc7d56c1df4daf2b472
Vashishth Gajjar <gajjarv2001@gmail.com>
2024-06-23 19:07:41 -0500

    integrated openAI and aggregation pipeline

---
+++ src/app/api/send-messages/route.ts
@@ -0,0 +1,47 @@
+import UserModel from '@/model/User';
+import dbConnect from '@/lib/dbConnect';
+import { Message } from '@/model/User';
+
+export async function POST(request: Request) {
+  await dbConnect();
+  const { name, content } = await request.json();
+
+  try {
+    const user = await UserModel.findOne({ name }).exec();
+
+    if (!user) {
+      return Response.json(
+        { message: 'User not found', success: false },
+        { status: 404 }
+      );
+    }
+
+    // Check if the user is accepting messages
+    if (!user.isAcceptingMessages) {
+      return Response.json(
+        { message: 'User is not accepting messages', success: false },
+        { status: 403 } 
+      );
+    }
+
+    const newMessage: Message = {
+      content: content,
+      createdAt: new Date()
+    } as unknown as Message;
+
+    // Push the new message to the user's messages array
+    user.messages.push(newMessage);
+    await user.save();
+
+    return Response.json(
+      { message: 'Message sent successfully', success: true },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.error('Error adding message:', error);
+    return Response.json(
+      { message: 'Internal server error', success: false },
+      { status: 500 }
+    );
+  }
+}
